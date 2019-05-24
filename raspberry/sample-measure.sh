418a77d82e7d285a91df34db64e1f614403936dd
safakoks <safakoksuzer@gmail.com>
2019-05-24 20:53:14 +0300

    sample script ADDED for raspberry

---
+++ raspberry/sample-measure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IP=$1
+PORT=$2
+
+while :
+do
+    curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.example.basic.EnergyUsageTransaction","asset": "org.example.basic.EnergyUsage#1000","AddingMeasure": '$(( ( RANDOM % 20 )  + 1 ))'}'  'http://'$IP':'$PORT'/api/org.example.basic.EnergyUsageTransaction'
+    
+    echo "Press [CTRL+C] to stop.."
+	sleep 5
+done
